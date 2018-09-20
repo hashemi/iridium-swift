69f399b5d53a1ff832ed59a4492c06641c6afd44
Ahmad Alhashemi <ahmad@ahmadh.com>
2018-09-20 22:32:42 +0300

    Initial Commit

---
+++ iridium-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iridium-swift
+//
+//  Created by Ahmad Alhashemi on 2018-09-20.
+//  Copyright © 2018 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
